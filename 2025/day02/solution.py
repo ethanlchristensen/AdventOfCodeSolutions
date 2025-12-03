cdab8f7673cc78c8ab69bf59f85742944268a646
ethanlchristensen <ethanlchristensen@outlook.com>
2025-12-02 22:01:25 -0600

    [update] - day 2 solutions

--- 2025/day02/solution.py
+++ 2025/day02/solution.py
@@ -2,6 +2,7 @@
 Advent of Code 2025 - Day 2
 """
 
+from sympy import divisors
 
 class Solution:
     def __init__(self, data_file="data"):
@@ -11,16 +12,35 @@ class Solution:
         """Load and parse the input data."""
         with open(filename, 'r') as f:
             return f.read().strip()
-    
+            
     def part1(self):
         """Solve part 1 of the puzzle."""
-        # TODO: Implement part 1
-        return None
+        return sum([0 if (len(str(v))%2!=0 or(str(v)[len(str(v))//2:]!=str(v)[:len(str(v))//2]))else v for l in[list(range(a,b+1))for a,b in[list(map(int, r.split("-")))for r in self.data.split(",")]]for v in l])
     
     def part2(self):
         """Solve part 2 of the puzzle."""
-        # TODO: Implement part 2
-        return None
+        total = 0
+
+        ids = [list(map(int, r.split("-")))for r in self.data.split(",")]
+
+        for low, high in ids:
+            for val in range(low, high + 1):
+                cs, csl = str(val), len(str(val))
+
+                if csl == 1: continue
+                
+                divs = divisors(csl)[1:-1]
+
+                if len(divs) == 0:
+                    if cs.count(cs[0]) == csl:
+                        total += val
+                else:
+                    for chunk_size in divs:
+                        section = cs[:chunk_size]
+                        if all(cs[i:i+chunk_size] == section for i in range(0, csl, chunk_size)):
+                            total += val
+                            break
+        return total
     
     def solve(self):
         """Run both parts and print results."""
