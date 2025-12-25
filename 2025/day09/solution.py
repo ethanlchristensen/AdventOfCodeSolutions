bf3f1e20ed10441758ef8b0e7520af026918ca3f
ethanlchristensen <ethanlchristensen@outlook.com>
2025-12-24 20:02:09 -0600

    [update] - day 9

--- 2025/day09/solution.py
+++ 2025/day09/solution.py
@@ -6,7 +6,7 @@ import math
 
 
 class Solution:
-    def __init__(self, data_file="datasmall"):
+    def __init__(self, data_file="data"):
         self.data = self.load_data(data_file)
 
     def load_data(self, filename):
@@ -17,35 +17,103 @@ class Solution:
             ]
 
     def print_board(self, board):
+        print(". " * (len(board[0]) + 2))
         for row in board:
+            print(". ", end="")
             for c in row:
                 print(f"{c} ", end="")
+            print(". ", end="")
             print()
+        print(". " * (len(board[0]) + 2))
         print()
 
     def part1(self):
         """Solve part 1 of the puzzle."""
-        width = max(x for x, y in self.data)
-        height = max(y for x, y in self.data)
+        rectangles = []
+        for y1, x1 in self.data:
+            for y2, x2 in [p for p in self.data if p != (x1, y1)]:
+                area = abs((x2 - x1 + 1) * (y2 - y1 + 1))
+                rectangles.append(area)
+        return max(rectangles)
 
-        board = [["." for _ in range(width + 1)] for __ in range(height + 1)]
+    def move_straight(self, dx, dy, x, y, board):
+        points = []
+        while 0 <= x + dx < len(board[0]) and 0 <= y + dy < len(board):
+            x += dx
+            y += dy
+            if board[y][x] == "#":
+                return points
+            points.append((x, y))
 
-        for x, y in self.data:
-            board[y][x] = "#"
+        return []
 
-        self.print_board(board)
+    def is_in_bounds(self, point, width):
+        if point in self.p2_valid_points:
+            return True
+        hits = 0
+        x, y = point
+        cur_x = x
+        while cur_x + 1 < width:
+            cur_x += 1
+            if (cur_x, y) in self.data:
+                return False
+            if (cur_x, y) in self.p2_valid_points:
+                hits += 1
 
-        for x, y in self.data:
-            hit = self.find_line_up(x, y, board)
-
-            print(hit)
-
-        return None
+        if hits == 0 or hits % 2 == 0:
+            return False
+        return True
 
     def part2(self):
         """Solve part 2 of the puzzle."""
-        # TODO: Implement part 2
-        return None
+        xs = [x for x, _ in self.data]
+        ys = [y for _, y in self.data]
+
+        width = max(xs) + 1
+        height = max(ys) + 1
+
+        x_data = {}
+        y_data = {}
+
+        for x, y in self.data:
+            if x not in x_data:
+                x_data[x] = [y]
+            else:
+                x_data[x].append(y)
+            if y not in y_data:
+                y_data[y] = [x]
+            else:
+                y_data[y].append(x)
+
+        valid_points = set()
+
+        for k, v in x_data.items():
+            start = min(v)
+            end = max(v)
+            for y in range(start, end + 1):
+                valid_points.add((k, y))
+
+        for k, v in y_data.items():
+            start = min(v)
+            end = max(v)
+            for x in range(start, end + 1):
+                valid_points.add((x, k))
+
+        print(f"Shape outline complete")
+        self.p2_valid_points = valid_points
+
+        rectangles = []
+        for y1, x1 in self.data:
+            for y2, x2 in [p for p in self.data if p != (x1, y1)]:
+                is_valid = True
+                corners = [(x1, y1), (x1, y2), (x2, y1), (x2, y2)]
+                for corner in corners:
+                    if not self.is_in_bounds(corner, width):
+                        is_valid = False
+                if is_valid:
+                    area = abs((x2 - x1 + 1) * (y2 - y1 + 1))
+                    rectangles.append(area)
+        return max(rectangles)
 
     def solve(self):
         """Run both parts and print results."""
